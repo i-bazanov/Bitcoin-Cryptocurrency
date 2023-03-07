a46dc0ebda4ab78209b916cb55a761f9519958c6
ibazanov <ilya.bazanov@phystech.edu>
2023-03-08 01:09:42 +0300

    fix code style after review 1.0

--- scroogecoin/main/UTXO.java
+++ scroogecoin/main/UTXO.java
@@ -44,11 +44,13 @@ public class UTXO implements Comparable<UTXO> {
         UTXO utxo = (UTXO) other;
         byte[] hash = utxo.txHash;
         int in = utxo.index;
-        if (hash.length != txHash.length || index != in)
+        if (hash.length != txHash.length || index != in) {
             return false;
+        }
         for (int i = 0; i < hash.length; i++) {
-            if (hash[i] != txHash[i])
+            if (hash[i] != txHash[i]) {
                 return false;
+            }
         }
         return true;
     }
@@ -68,23 +70,29 @@ public class UTXO implements Comparable<UTXO> {
     public int compareTo(UTXO utxo) {
         byte[] hash = utxo.txHash;
         int in = utxo.index;
-        if (in > index)
+        if (in > index) {
             return -1;
-        else if (in < index)
+        }
+        else if (in < index) {
             return 1;
+        }
         else {
             int len1 = txHash.length;
             int len2 = hash.length;
-            if (len2 > len1)
+            if (len2 > len1) {
                 return -1;
-            else if (len2 < len1)
+            }
+            else if (len2 < len1) {
                 return 1;
+            }
             else {
                 for (int i = 0; i < len1; i++) {
-                    if (hash[i] > txHash[i])
+                    if (hash[i] > txHash[i]) {
                         return -1;
-                    else if (hash[i] < txHash[i])
+                    }
+                    else if (hash[i] < txHash[i]) {
                         return 1;
+                    }
                 }
                 return 0;
             }
